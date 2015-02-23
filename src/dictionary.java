e80f048ed7dfd242a05ed28b2e598170a3a3afa8
1302435 <1302435@RG43033.rgu.ac.uk>
2015-02-23 09:35:36 +0000

    A simple program for dealing with customers and visitors

---
+++ src/dictionary.java
@@ -0,0 +1,17 @@
+import java.util.Dictionary;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author 1302435
+ * @version 23/02/2015
+ */
+public interface dictionary {
+    
+    public String getDefinition(String str);
+   
+}
